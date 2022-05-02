89b29f50c31dbf130541408ec5f73d1897af4b42
emmanueles1993 <emmanueles1993@hotmail.com>
2022-05-02 17:12:49 -0500

    create views

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,15 @@
 from django.urls import URLPattern, path
 
 from . import views
-
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index")
+    #example /polls/
+    path("", views.index, name="index"),
+    #example /polls/5/
+    path("<int:question_id>/detail/estaeslamejorpagina", views.detail, name="detail"),
+    #ex: /polls/results
+    path("<int:question_id>/results/", views.results, name="results"),
+    #ex /polls/5/vote
+    path("<int:question_id>/vote", views.vote, name="vote"),
+
 ]
